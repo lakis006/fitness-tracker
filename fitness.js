124affe1aa20a40d747acc816cf678aca4e3c90b
Jsquad <lakis006@umn.edu>
2020-07-30 18:09:46 -0500

    finished adding the schema in fitness.js

--- fitness.js
+++ fitness.js
@@ -10,5 +10,28 @@ const FitnessSchema = new Schema({
             trim: true,
             required: "You need to select a type!"
         },
+        name:  {
+            type: String,
+            trim: true,
+            required: "You need to select a Workout!"
+        },
+        duration: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        }, 
+        reps: {
+            type: Number
+        }
     }]
-})
+});
+const Fitness = mongoose.model("fitness", FitnessSchema)
+
+module.exports = Fitness;
