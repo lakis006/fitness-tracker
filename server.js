286ad50f5aa2deabe095c85a02fa0fae4e04c36a
Jsquad <lakis006@umn.edu>
2020-08-10 01:08:07 -0500

    finalized server.js APP methods to run app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const logger = require("logger");
 const express = require("express");
 
-let fitness = require("../fitness.js"); 
+let Fitness = require("../fitness.js"); 
 
 
 const PORT = process.env.PORT || 3005;
@@ -19,3 +19,44 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_ATLAS_URI || "mongodb://localhost/fitness", {useNewUrlParser: true });
+
+app.get("/api/workouts", (req, res) =>{
+    Fitness.find({}).then((fitnessDB)=>{
+        res.json(fitnessDB);
+    })
+});
+
+app.put("/api/workouts/:id", (req, res) =>{
+    Fitness.update({"_id": req.params.id},{
+        $push:{exercises: req.body}
+    })
+    .then((fitnessDB) =>{
+        res.json(fitnessDB)
+    });
+});
+
+app.post("/api/workouts/", (req, res) =>{
+    Fitness.create(req)
+    .then((fitnessDB) =>{
+        res.json(fitnessDB)
+    });
+});
+
+app.get("/api/workouts/range", (req, res) =>{
+    Fitness.find({})
+    .then((fitnessDB)=>{
+        res.json(fitnessDB)
+    });
+});
+
+app.get("/stats", (req,res) =>{
+    res.sendFile(path.join(__dirname + 'public/stats.html'));
+});
+
+app.get("/exercise", (req, res) =>{
+    res.sendFile(path.join(__dirname + 'public/exercise.html'));
+});
+
+app.listen(PORT, ()=>{
+    console.log(`App running on ${PORT}: `)
+})
